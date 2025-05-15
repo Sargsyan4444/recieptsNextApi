d72ce68fef5c99782d5b63dbcc007484a093a7c9
Sargsyan4444 <ruzan.sargsyan.444@gmail.com>
2025-05-15 20:23:56 +0300

    i added reciepts cards

---
+++ app/components/card.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+export interface CardProps {
+  id: number;
+  name: string;
+  caloriesPerServing: number;
+  image: string;
+  mealType: string[];
+}
+
+function Card({
+  id,
+  name,
+  caloriesPerServing,
+  image,
+  mealType,
+}: CardProps) {
+  return (
+    <div className="w-[400px] h-[600px] rounded-2xl bg-orange-400 flex flex-col items-center gap-5">
+      <img className="w-[400px] rounded-2xl h-[300px]  object-cover" src={image} alt={name} />
+      <div className="flex flex-col gap-3 pt-3">
+      <h2>
+        <Link className="text-white font-bold text-3xl hover:text-black" href="">{name}</Link>
+      </h2>
+
+      <p><strong className="font-medium text-amber-50 ">Meal Types:</strong></p>
+      <ul>
+        {mealType.map((elm, idx) => (
+          <li className="font-medium text-amber-50" key={idx}>{elm}</li>
+        ))}
+      </ul>
+
+      <p className="font-medium text-amber-50"><strong className=" text-amber-50">Calories per Serving:</strong> {caloriesPerServing}</p>
+    </div>
+      </div>
+  );
+}
+
+export { Card };
--- app/reciepts/page.tsx
+++ app/reciepts/page.tsx
@@ -1,6 +1,24 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { Card, CardProps } from "../components/card";
+
 function RecieptsPage() {
+    const [data,setData] = useState([])
+
+    useEffect(()=>{
+        fetch('https://dummyjson.com/recipes')
+        .then(res => res.json())
+        .then(res=>setData(res.recipes));
+    })
+
     return (
-        <h1>reciepts page</h1>
+      <div>
+        <h1 className="text-center text-orange-400 text-6xl font-bold p-4 ">Reciepts Page</h1>
+        <div className="flex flex-wrap justify-center items-center gap-4">
+            {data.map((elm:CardProps)=><Card key={elm.id} name={elm.name} caloriesPerServing={elm.caloriesPerServing} image={elm.image} mealType={elm.mealType} id={elm.id}/>)}
+        </div>
+      </div>
     )
 }
 export default RecieptsPage
