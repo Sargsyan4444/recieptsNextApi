54b1ce5f390e9b9f8f620d845fd3e37370b5f83a
Sargsyan4444 <ruzan.sargsyan.444@gmail.com>
2025-05-15 20:39:24 +0300

    i added search part

--- app/reciepts/page.tsx
+++ app/reciepts/page.tsx
@@ -4,17 +4,37 @@ import { useEffect, useState } from "react"
 import { Card, CardProps } from "../components/card";
 
 function RecieptsPage() {
-    const [data,setData] = useState([])
+    const [data, setData] = useState<CardProps[]>([])
+    const [search,setSearch] = useState('')
 
-    useEffect(()=>{
-        fetch('https://dummyjson.com/recipes')
-        .then(res => res.json())
-        .then(res=>setData(res.recipes));
-    })
-
+    useEffect(() => {
+        const delayDebounce = setTimeout(() => {
+          const url = search.trim()
+            ? `https://dummyjson.com/recipes/search?q=${search}`
+            : `https://dummyjson.com/recipes`;
+      
+          fetch(url)
+            .then(res => res.json())
+            .then(json => setData(json.recipes || []))
+            .catch(error => console.error("Failed to fetch recipes:", error));
+        }, 500); 
+      
+        return () => clearTimeout(delayDebounce);
+      }, [search]);
+      
+  
     return (
       <div>
         <h1 className="text-center text-orange-400 text-6xl font-bold p-4 ">Reciepts Page</h1>
+        <div className="text-center mb-6">
+        <input
+          type="search"
+          placeholder="Search recipes..."
+          className="border px-4 py-2 rounded shadow bg-orange-400 border-amber-50 placeholder:text-zinc-100"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
         <div className="flex flex-wrap justify-center items-center gap-4">
             {data.map((elm:CardProps)=><Card key={elm.id} name={elm.name} caloriesPerServing={elm.caloriesPerServing} image={elm.image} mealType={elm.mealType} id={elm.id}/>)}
         </div>
